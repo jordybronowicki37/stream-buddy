de726c212503c2a040292830b9503294e7e6df62
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2025-06-03 21:51:59 +0200

    Improved state machine of the streamer checking logic

--- src/streamer.py
+++ src/streamer.py
@@ -1,7 +1,15 @@
+from enum import Enum
 from time import sleep
 from datetime import datetime, timedelta
 
 from playwright.sync_api import Page
+
+
+class OnlineStatus(Enum):
+    UNKNOWN = 0
+    OFFLINE = 1
+    GRACE_PERIOD = 2
+    ONLINE = 3
 
 
 class Streamer:
@@ -9,19 +17,13 @@ class Streamer:
         self.name = name
         self.url = url
         self.selector = selector
-        self._live = False
+        self.status = OnlineStatus.UNKNOWN
         self._just_live = False
         self._just_offline = False
-        self._last_live_status = False
         self._last_positive_live_check: datetime | None = None
 
     def is_live(self):
-        # Always return true if the last online check was less than 5 minutes ago
-        if self._last_positive_live_check is not None and datetime.now() - self._last_positive_live_check > timedelta(minutes=5):
-            self._last_live_status = True
-            return True
-        self._last_live_status = self._live
-        return self._live
+        return self.status == OnlineStatus.ONLINE or self.status == OnlineStatus.GRACE_PERIOD
 
     def is_just_live(self):
         if self._just_live:
@@ -36,24 +38,32 @@ class Streamer:
         return False
 
     def check_live(self, page: Page):
-        last_live_status = self._last_live_status
+        # Checks if the user was seen online in the past 5 minutes
+        recently_online = self._last_positive_live_check is not None and datetime.now() - self._last_positive_live_check > timedelta(minutes=5)
+        last_status = self.status
         was_offline = not self.is_live()
         is_live = False
 
+        # Reset recent change values
+        self._just_live = False
+        self._just_offline = False
+
+        # Check the live status in intervals
         page.goto(self.url)
-        sleep(1)
         for i in range(20):
-            sleep(0.2)
+            sleep(0.5)
             live_indicator = page.query_selector(self.selector)
             is_live = live_indicator is not None
-
             if is_live:
-                self._last_positive_live_check = datetime.now()
-                if was_offline:
-                    self._just_live = True
                 break
 
-        if not is_live and last_live_status:
+        if is_live:
+            self._last_positive_live_check = datetime.now()
+            if was_offline:
+                self._just_live = True
+            self.status = OnlineStatus.ONLINE
+        elif last_status == OnlineStatus.ONLINE:
+            self.status = OnlineStatus.GRACE_PERIOD
+        elif last_status == OnlineStatus.GRACE_PERIOD and not recently_online:
             self._just_offline = True
-
-        self._live = is_live
+            self.status = OnlineStatus.OFFLINE

f0eeeaa734081f01c5f5e033e51345a56f17f3fc
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2025-07-07 19:24:46 +0200

    Fixed json parsing

--- src/data_handler.py
+++ src/data_handler.py
@@ -33,5 +33,5 @@ def add_stream_log(streamer: str, start_time: datetime, end_time: datetime):
     stream_file = Path(f"{stream_dir}/streams.jsonl")
     stream_file.touch()
     with jsonlines.open(stream_file, mode="a") as writer:
-        writer.write({"start_time": start_time, "end_time": end_time})
+        writer.write({"start_time": str(start_time), "end_time": str(end_time)})
 

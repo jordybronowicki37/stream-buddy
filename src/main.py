bee11d0df00325cd2cf0462eab05ede5348042e2
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2025-07-11 23:05:52 +0200

    Separated main into smaller steps and added SIGTERM signal listener

--- src/main.py
+++ src/main.py
@@ -1,59 +1,79 @@
+import logging
+import sys
 from os import getenv
+import signal
+
 import requests
-
 from dotenv import load_dotenv
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 
+from data_handler import get_streamers, add_stream_log
 from discord_notification import send_online_notification, send_offline_notification
 from scheduling import extend_function_runtime
-from data_handler import get_streamers, add_stream_log
 from streamer import Streamer
 
 
-def check_streamers(ls: list[Streamer]):
+def handle_sigterm(signum, frame):
+    logger.info('Received signal to terminate.')
+    # TODO save streamer data before shutdown
+    sys.exit(0)
+
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+signal.signal(signal.SIGTERM, handle_sigterm)
+
+
+def load_environment():
+    logger.info("Loading environment")
+    load_dotenv()
+    webhook_url = getenv("DISCORD_WEBHOOK_URL")
+    if not webhook_url:
+        logger.info("No webhook URL set")
+        sys.exit(1)
+    result = requests.get(webhook_url)
+    if result.status_code != 200:
+        logger.info("Webhook URL is not valid")
+        sys.exit(1)
+
+
+def check_streamers(page: Page, ls: list[Streamer]):
     for s in ls:
         s.check_live(page)
         if s.is_just_live():
-            print(f"Streamer {s.name} is live!")
+            logger.info(f"Streamer {s.name} is live!")
             send_online_notification(s)
         elif s.is_just_offline():
-            print(f"Streamer {s.name} just went offline!")
+            logger.info(f"Streamer {s.name} just went offline!")
             stream_log = s.get_stream_log()
             if stream_log is not None:
                 add_stream_log(s.name, stream_log.get("start_time"), stream_log.get("end_time"))
             send_offline_notification(s)
 
 
-if __name__ == "__main__":
-    print("Loading environment")
-    load_dotenv()
-    webhook_url = getenv("DISCORD_WEBHOOK_URL")
-    if not webhook_url:
-        print("No webhook URL set")
-        exit(1)
-    result = requests.get(webhook_url)
-    if result.status_code != 200:
-        print("Webhook URL is not valid")
-        exit(1)
-
-    # Read followed streamers data file
-    print("Retrieving streamers")
-    streamers = get_streamers()
-
-    # Start browser
-    print("Starting browser")
+def start_browser():
+    logger.info("Starting browser")
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
 
+    # Read followed streamers data file
+    logger.info("Retrieving streamers")
+    streamers = get_streamers()
+
     try:
         # Check loop
-        print("Starting application loop")
+        logger.info("Starting application loop")
         while True:
-            extend_function_runtime(60, lambda: check_streamers(streamers))
+            extend_function_runtime(60, lambda: check_streamers(page, streamers))
     except KeyboardInterrupt:
-        print("Interrupted. Cleaning up.")
+        logger.error("Interrupted. Cleaning up.")
     finally:
         browser.close()
         p.stop()
+
+
+if __name__ == "__main__":
+    load_environment()
+    start_browser()
